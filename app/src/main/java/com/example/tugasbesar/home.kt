b297f22987f3013697ab400288215c7c2396ed16
Winsdtrom5 <anggagant@gmail.com>
2022-09-08 07:15:48 +0700

    TgsBesar

--- app/src/main/java/com/example/tugasbesar/home.kt
+++ app/src/main/java/com/example/tugasbesar/home.kt
@@ -1,11 +1,41 @@
 package com.example.tugasbesar
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = MenuInflater(this)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menulogout) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this@home)
+            builder.setMessage("Are You Sure Wanna Logout??")
+                .setNegativeButton("No", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface, i: Int) {
+
+                    }
+                })
+                .setPositiveButton("YES", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface, i: Int) {
+                        finishAndRemoveTask()
+                    }
+                })
+                .show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
