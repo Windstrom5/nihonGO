cddd1c28602f8d28ee868afef25e08f8ee2088b3
Winsdtrom5 <anggagant@gmail.com>
2022-09-22 13:42:54 +0700

    TgsBesar

---
+++ app/src/main/java/com/example/tugasbesar/FragmentProfile.kt
@@ -0,0 +1,33 @@
+package com.example.tugasbesar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tugasbesar.entity.eventTokyo
+
+class FragmentEvent : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        val layoutManager = LinearLayoutManager(context)
+//        RVEventAdapter.layoutManager = layoutManager
+//        RVEventAdapter .setHasFixedSize(true)
+//        RVEventAdapter.adapter = adapter
+    }
+
+    companion object {
+
+    }
+}
