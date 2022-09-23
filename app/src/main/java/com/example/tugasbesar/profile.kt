983e3f483610b37894b20d3f7b6a9f53162da1cd
AntioMood <brigita079@gmail.com>
2022-09-23 10:24:26 +0700

    kerjain dikit dikit

---
+++ app/src/main/java/com/example/tugasbesar/profile.kt
@@ -0,0 +1,98 @@
+package com.example.tugasbesar
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.tugasbesar.room.User
+import com.example.tugasbesar.room.UserDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class profile : AppCompatActivity() {
+    val db by lazy { UserDB(this) }
+    lateinit var Users: MainAdapterProfile
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadData()
+//        setupRecyclerView()
+    }
+
+//    private fun setupRecyclerView() {
+//        Users = NoteAdapter(arrayListOf(), object : NoteAdapter.OnAdapterListener{
+//            override fun onClick(note: Note) {
+//                //Toast.makeText(applicationContext, note.title, Toast.LENGTH_SHORT).show()
+//                intentEdit(note.id, Constant.TYPE_READ)
+//            }
+//
+//            override fun onUpdate(note: Note) {
+//                intentEdit(note.id, Constant.TYPE_UPDATE)
+//            }
+//
+//            override fun onDelete(note: Note) {
+//                deleteDialog(note)
+//            }
+//        })
+//        list_note.apply {
+//            layoutManager = LinearLayoutManager(applicationContext)
+//            adapter = noteAdapter
+//        }
+//    }
+
+    private fun deleteDialog(user: User){
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.apply {
+            setTitle("Confirmation")
+            setMessage("Are You Sure to delete this data From ${user.username}?")
+            setNegativeButton("Cancel", DialogInterface.OnClickListener{
+                    dialogInterface, i -> dialogInterface.dismiss()
+            })
+            setPositiveButton("Delete", DialogInterface.OnClickListener{
+                    dialogInterface, i -> dialogInterface.dismiss()
+                CoroutineScope(Dispatchers.IO).launch {
+                    db.noteDao().deleteNote(user)
+                    loadData()
+                }
+            })
+        }
+        alertDialog.show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadData()
+    }
+
+    fun loadData() {
+        CoroutineScope(Dispatchers.IO).launch  {
+            val notes = db.noteDao().getNotes()
+            Log.d("MainActivity","dbResponse: $notes")
+            withContext(Dispatchers.Main){
+                Users.setData( notes )
+            }
+        }
+    }
+
+
+//    fun setupListener() {
+//        button_create.setOnClickListener{
+//            intentEdit(0,Constant.TYPE_CREATE)
+//        }
+//    }
+
+//    fun intentEdit(noteId : Int, intentType: Int){
+//        startActivity(
+//            Intent(applicationContext, EditActivity::class.java).putExtra("intent_id", noteId).putExtra("intent_type", intentType)
+//        )
+//    }
+}
