0afba90031ca31636c184c488b1246dd0bb5ae89
Winsdtrom5 <anggagant@gmail.com>
2022-10-02 21:05:04 +0700

    TgsBesar

--- app/src/main/java/com/example/tugasbesar/EditActivity.kt
+++ app/src/main/java/com/example/tugasbesar/EditActivity.kt
@@ -63,8 +63,8 @@ class EditActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
             }
             val intent = Intent(this,profile::class.java)
             val mBundle = Bundle()
-            mBundle.putString("username",usernameEdit.toString())
-            mBundle.putString("password",passwordEdit.toString())
+            mBundle.putString("username",usernameEdit.getText().toString())
+            mBundle.putString("password",passwordEdit.getText()12.toString())
             intent.putExtra("profile",mBundle)
             startActivity(intent)
         }
