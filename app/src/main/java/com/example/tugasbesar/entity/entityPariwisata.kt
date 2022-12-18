605849359b1c1907fff5c55f5ed94a8815a68529
Dave <davehabelpaprindey@gmail.com>
2022-12-18 13:22:03 +0700

    AmbatuCom(mit) list nama pariwisata

--- app/src/main/java/com/example/tugasbesar/entity/entityPariwisata.kt
+++ app/src/main/java/com/example/tugasbesar/entity/entityPariwisata.kt
@@ -6,10 +6,10 @@ class entityPariwisata(var nama : Int, var id :String) {
     companion object{
         @JvmField
         var listnamaPariwisata = arrayOf(
-            entityPariwisata(R.drawable.akomodasilogo,"Akomodasi"),
-            entityPariwisata(R.drawable.culinary_logo,"Kuliner"),
-            entityPariwisata(R.drawable.wisatabig,"Wisata"),
-            entityPariwisata(R.drawable.eventbig,"Event"),
+            entityPariwisata(R.drawable.txtaccomodation,"Akomodasi"),
+            entityPariwisata(R.drawable.txtcullinary,"Kuliner"),
+            entityPariwisata(R.drawable.txttourist,"Wisata"),
+            entityPariwisata(R.drawable.txtevent,"Event"),
         )
     }
 }
